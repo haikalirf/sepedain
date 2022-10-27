4464537d6a4328066dadb6433fe1e6e573cbff3f
damieroliverroot <87322409+damieroliverroot@users.noreply.github.com>
2022-10-27 20:48:28 +0700

    Added Fragment

--- app/src/main/java/com/example/sepedain/home/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/sepedain/home/ui/map/MapFragment.kt
@@ -1,4 +1,4 @@
-package com.example.sepedain.home.ui.dashboard
+package com.example.sepedain.home.ui.map
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.sepedain.databinding.FragmentDashboardBinding
+import com.example.sepedain.databinding.FragmentMapBinding
 
-class DashboardFragment : Fragment() {
+class MapFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentMapBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(MapViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
