cdd630867bdc1030ce2a1259271f41465bcaeae2
damieroliverroot <87322409+damieroliverroot@users.noreply.github.com>
2022-10-29 05:15:36 +0700

    fixed fetch bugs

--- app/src/main/java/com/example/sepedain/main/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sepedain/main/ui/home/HomeFragment.kt
@@ -31,6 +31,42 @@ class HomeFragment : Fragment() {
         return root
     }
 
+<<<<<<< Updated upstream
+=======
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val client = ApiClient.apiService.fetchPlace(
+            "commercial",
+            "geometry:0ad2ca57a82b4a5ab2919dc0a5e93711",
+            "proximity:112.6659335,-7.8931151",
+            "20",
+            "ec51bcde20554127ac97cc4c52eff067"
+        )
+
+        client.enqueue(object : retrofit2.Callback<PlaceResponse> {
+            override fun onResponse(call: Call<PlaceResponse>, response: Response<PlaceResponse>) {
+                if (response.isSuccessful) {
+                    Log.d("place", ""+response.body())
+
+                    val result = response.body()?.result
+                    result?.let {
+                        val adapter = BikesNearYouAdapter(result)
+                        val recyclerView = binding.rvBikesnearyou
+                        recyclerView.layoutManager = LinearLayoutManager(HomeFragment().context, LinearLayoutManager.HORIZONTAL, false)
+                        recyclerView.adapter = adapter
+                        recyclerView.setHasFixedSize(true)
+//                        binding.tvHellouserHomefragment.text = result[0].name
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<PlaceResponse>, t: Throwable) {
+                Log.e("failed", ""+t.message)
+            }
+        })
+    }
+
+>>>>>>> Stashed changes
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
---
+++ app/src/main/java/com/example/sepedain/network/Place.kt
@@ -0,0 +1,30 @@
+package com.example.sepedain.network
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Place(
+//    @Json(name = "name")
+//    val name: String,
+//    @Json(name = "lon")
+//    val lon: Double,
+//    @Json(name = "lat")
+//    val lat: Double,
+//    @Json(name = "formatted")
+//    val formatted: String
+    @Json(name = "properties")
+    val properties : Properties
+) : Parcelable
+
+@Parcelize
+data class Properties (
+    @Json(name = "name")
+    val name : String
+) : Parcelable
+
+data class PlaceResponse(
+    @Json(name = "features")
+    val result: List<Place>
+)
