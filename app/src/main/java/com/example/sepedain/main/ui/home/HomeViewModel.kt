4cbbedf860a77c653be3441af5252648f9fe8f64
damieroliverroot <87322409+damieroliverroot@users.noreply.github.com>
2022-10-29 12:17:28 +0700

    fixed bikesnearyou bug

--- app/src/main/java/com/example/sepedain/main/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/sepedain/main/ui/home/HomeViewModel.kt
@@ -1,11 +1,16 @@
 package com.example.sepedain.main.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.sepedain.main.Repository
 import com.example.sepedain.network.ApiClient
 import com.example.sepedain.network.Place
+import com.example.sepedain.network.PlaceResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeViewModel(
     private val repository: Repository
@@ -15,7 +20,30 @@ class HomeViewModel(
     val placeLiveData: LiveData<List<Place>>
         get() = _placesLiveData
 
-    private fun fetchPlace() {
+    init {
+        fetchPlace()
+    }
 
+    private fun fetchPlace() {
+        val client = repository.getPlaces(
+            "commercial",
+            "geometry:0ad2ca57a82b4a5ab2919dc0a5e93711",
+//            "proximity:${HomeFragment().longitude},${HomeFragment().latitude}",
+            "proximity:112.6240242,-7.960171",
+            "20",
+            "ec51bcde20554127ac97cc4c52eff067"
+        )
+        client.enqueue(object : Callback<PlaceResponse> {
+            override fun onResponse(call: Call<PlaceResponse>, response: Response<PlaceResponse>) {
+                if(response.isSuccessful) {
+                    Log.d("place", "" + response.body())
+                    _placesLiveData.postValue(response.body()?.result)
+                }
+            }
+
+            override fun onFailure(call: Call<PlaceResponse>, t: Throwable) {
+                Log.d("Failure", t.message.toString())
+            }
+        })
     }
 }
